a815b74022b11320d594f4e4ebb44257b6f33bcb
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-28 11:36:38 +0700

    #20 - Add more details in README.md file :fire: :blossom:

---
+++ README.md
@@ -0,0 +1,9 @@
+# Spring Boot One To Many
+
+### Things todo list:
+
+1. Clone this repository: `git clone https://gitlab.com/hendisantika/spring-boot-one-to-many.git`
+2. Navigate to the folder: `cd spring-boot-one-to-many`
+3. Change MySQL credentials with your own in `application.properties` file
+4. Run application: `mvn clean spring-boot:run`
+5. Open Postman App or import the Postman collection file
--- src/main/java/com/hendisantika/entity/AuditModel.java
+++ src/main/java/com/hendisantika/entity/AuditModel.java
@@ -1,6 +1,7 @@
 package com.hendisantika.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -29,6 +30,7 @@ import java.util.Date;
         value = {"createdAt", "updatedAt"},
         allowGetters = true
 )
+@Data
 public abstract class AuditModel implements Serializable {
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "created_at", nullable = false, updatable = false)
