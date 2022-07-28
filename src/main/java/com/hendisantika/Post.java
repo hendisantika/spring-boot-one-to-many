e9fc7790d645053bc2d01b137f64a457c0b8b269
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-28 10:08:07 +0700

    #5 - Add Post Entity :pencil2: :green_book:

---
+++ src/main/java/com/hendisantika/Post.java
@@ -0,0 +1,49 @@
+package com.hendisantika;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-one-to-many
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 7/28/22
+ * Time: 10:06
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "posts")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post extends AuditModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(max = 100)
+    @Column(unique = true)
+    private String title;
+
+    @NotNull
+    @Size(max = 250)
+    private String description;
+
+    @NotNull
+    @Lob
+    private String content;
+}
