1759faa091c73d9970432093796dfe5ce1734a51
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-28 10:16:38 +0700

    #15 - Add PostController :pencil2: :green_book:

---
+++ src/main/java/com/hendisantika/controller/PostController.java
@@ -0,0 +1,25 @@
+package com.hendisantika.controller;
+
+import com.hendisantika.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-one-to-many
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 7/28/22
+ * Time: 10:16
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class PostController {
+
+    private final PostRepository postRepository;
+
+}
