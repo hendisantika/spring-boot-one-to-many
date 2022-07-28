b5382b436df91183180d0db77960bbc83f591aa8
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-28 10:05:45 +0700

    #4 - Add AuditModel Entity :pencil2: :green_book:

---
+++ src/main/java/com/hendisantika/AuditModel.java
@@ -0,0 +1,42 @@
+package com.hendisantika;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-one-to-many
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 7/28/22
+ * Time: 10:04
+ * To change this template use File | Settings | File Templates.
+ */
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(
+        value = {"createdAt", "updatedAt"},
+        allowGetters = true
+)
+public abstract class AuditModel implements Serializable {
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreatedDate
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at", nullable = false)
+    @LastModifiedDate
+    private Date updatedAt;
+}
