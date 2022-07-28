92c260ccb0df500615f4e6b77b50c38e9074c6f0
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-28 10:17:27 +0700

    #17 - Add createPost API in PostController :pencil2: :green_book:

--- src/main/java/com/hendisantika/controller/PostController.java
+++ src/main/java/com/hendisantika/controller/PostController.java
@@ -7,7 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,5 +33,10 @@ public class PostController {
     @GetMapping("/posts")
     public Page<Post> getAllPosts(Pageable pageable) {
         return postRepository.findAll(pageable);
+    }
+
+    @PostMapping("/posts")
+    public Post createPost(@Valid @RequestBody Post post) {
+        return postRepository.save(post);
     }
 }
