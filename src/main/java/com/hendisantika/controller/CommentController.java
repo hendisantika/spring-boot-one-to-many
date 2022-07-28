5e38e5f42a4113986979a93b7dccefdfdf986a8d
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-28 10:15:32 +0700

    #13 - Add updateComment API in CommentController :pencil2: :green_book:

--- src/main/java/com/hendisantika/controller/CommentController.java
+++ src/main/java/com/hendisantika/controller/CommentController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -48,5 +49,19 @@ public class CommentController {
             comment.setPost(post);
             return commentRepository.save(comment);
         }).orElseThrow(() -> new ResourceNotFoundException("PostId " + postId + " not found"));
+    }
+
+    @PutMapping("/posts/{postId}/comments/{commentId}")
+    public Comment updateComment(@PathVariable(value = "postId") Long postId,
+                                 @PathVariable(value = "commentId") Long commentId,
+                                 @Valid @RequestBody Comment commentRequest) {
+        if (!postRepository.existsById(postId)) {
+            throw new ResourceNotFoundException("PostId " + postId + " not found");
+        }
+
+        return commentRepository.findById(commentId).map(comment -> {
+            comment.setText(commentRequest.getText());
+            return commentRepository.save(comment);
+        }).orElseThrow(() -> new ResourceNotFoundException("CommentId " + commentId + "not found"));
     }
 }
