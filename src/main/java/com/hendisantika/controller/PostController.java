2c2d3df2e4a53197431e538fbd307725183ef355
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-28 10:17:44 +0700

    #18 - Add updatePost API in PostController :pencil2: :green_book:

--- src/main/java/com/hendisantika/controller/PostController.java
+++ src/main/java/com/hendisantika/controller/PostController.java
@@ -1,13 +1,16 @@
 package com.hendisantika.controller;
 
 import com.hendisantika.entity.Post;
+import com.hendisantika.exception.ResourceNotFoundException;
 import com.hendisantika.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,5 +41,15 @@ public class PostController {
     @PostMapping("/posts")
     public Post createPost(@Valid @RequestBody Post post) {
         return postRepository.save(post);
+    }
+
+    @PutMapping("/posts/{postId}")
+    public Post updatePost(@PathVariable Long postId, @Valid @RequestBody Post postRequest) {
+        return postRepository.findById(postId).map(post -> {
+            post.setTitle(postRequest.getTitle());
+            post.setDescription(postRequest.getDescription());
+            post.setContent(postRequest.getContent());
+            return postRepository.save(post);
+        }).orElseThrow(() -> new ResourceNotFoundException("PostId " + postId + " not found"));
     }
 }
